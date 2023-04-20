d32889716c74a27e54f059cb032b24f84335c83d
mostafa2080 <mostafashaaban2080@gmail.com>
2023-04-20 20:48:03 +0200

    Area & Pharmacy Features Added

--- app/Http/Controllers/AreaController.php
+++ app/Http/Controllers/AreaController.php
@@ -2,33 +2,28 @@
 
 namespace App\Http\Controllers;
 
-use App\Models\Pharmacy;
+use App\Models\Area;
 use Illuminate\Http\Request;
 use DataTables;
 
-class PharmacyController extends Controller
+class AreaController extends Controller
 {
     public function index(Request $request)
     {
 
         if ($request->ajax()) {
-            $data = Pharmacy::select('id', 'priority', 'owner_user_id', 'area_id')->get();
+            $data = Area::select('id', 'name', 'address')->get();
             return Datatables::of($data)->addIndexColumn()
                 ->addColumn('action', function ($row) {
                     $btn = '<a href="javascript:void(0)" class="btn btn-success btn-sm mx-2">View</a>';
                     $btn .= '<a href="javascript:void(0)" class="btn btn-primary btn-sm mx-2">Edit</a>';
                     $btn .= '<a href="javascript:void(0)" class="btn btn-danger btn-sm">Delete</a>';
                     return $btn;
-                })->addColumn('Name',function(Pharmacy $pharmacy){
-                    return $pharmacy->user->name;
-                })->addColumn('area',function(Pharmacy $pharmacy){
-                    return $pharmacy->area->name;
                 })
-                ->rawColumns(['action', 'Name'])
+                ->rawColumns(['action'])
                 ->make(true);
         }
 
-        return view('admin.Pharmacy.index');
+        return view('admin.Area.index');
     }
 }
-
