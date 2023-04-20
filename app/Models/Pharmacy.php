0d278c369f7fc4a6b438f9e4593b08b0bae5ba0b
mostafa2080 <mostafashaaban2080@gmail.com>
2023-04-20 22:23:09 +0200

    fixed conflict in pharmacy model

--- app/Models/Pharmacy.php
+++ app/Models/Pharmacy.php
@@ -8,11 +8,7 @@ use Illuminate\Database\Eloquent\Model;
 class Pharmacy extends Model
 {
     use HasFactory;
-<<<<<<< HEAD
-<<<<<<< HEAD
 
-=======
->>>>>>> 8d0e4480603ac6e605bb79638a364bcf115d4254
         protected $fillable = [
             'priority',
             'owner_user_id',
@@ -20,10 +16,6 @@ class Pharmacy extends Model
             'name'
         ];
 
-<<<<<<< HEAD
-
-=======
->>>>>>> 8d0e4480603ac6e605bb79638a364bcf115d4254
     public function area()
     {
         return $this->belongsTo(Area::class, 'area_id');
@@ -33,10 +25,5 @@ class Pharmacy extends Model
     {
         return $this->belongsTo(User::class, 'owner_user_id');
     }
-<<<<<<< HEAD
 
-=======
->>>>>>> e89560f (edit order details table)
-=======
->>>>>>> 8d0e4480603ac6e605bb79638a364bcf115d4254
 }
