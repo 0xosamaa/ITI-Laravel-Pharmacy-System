e89560f7119f8dedfc41d0c2c0db48e7fe465f8d
hassanOsama720 <hassan.osama720@gmail.com>
2023-04-20 17:36:27 +0200

    edit order details table

---
+++ database/migrations/2023_04_20_151815_add_columns_to_order_details_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('order_details', function (Blueprint $table) {
+            $table->foreignId('doctor_id')->constrained('users');
+            $table->foreignId('pharmacy_id')->constrained('pharmacies');
+            $table->enum('status', ['New', 'Confirmed', 'Processing', 'WaitingForUserConfirmation', 'Completed', 'Cancelled']);
+            $table->boolean('is_insured');
+            $table->string('delivery_address');
+            $table->string('creator_type');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('order_details', function (Blueprint $table) {
+            //
+        });
+    }
+};
