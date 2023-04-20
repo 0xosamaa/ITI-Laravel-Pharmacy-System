ac5bac1f4adb7e69b38432f0d64510be44df4a86
amrbasiony97 <amrbasiony97@gmail.com>
2023-04-21 00:17:51 +0200

    Fixed migration bugs

--- database/migrations/2023_04_20_221248_create_doctors_info_table.php
+++ database/migrations/2023_04_20_221248_create_doctors_info_table.php
@@ -13,11 +13,11 @@ return new class extends Migration
     {
         Schema::create('doctors_info', function (Blueprint $table) {
             $table->id();
-            $table->foreign('doctor_id')->references('id')->on('users');
+            $table->foreignId('doctor_id')->constrained('users');
             $table->decimal('national_id', $precision = 14, $scale = 0)->unique();
             $table->string('avatar_image')->default('default.jpg');
             $table->boolean('is_banned')->default(false);
-            $table->foreign('pharmacy_id')->references('id')->on('pharmacies');
+            $table->foreignId('pharmacy_id')->constrained('pharmacies');
             $table->timestamps();
         });
     }
