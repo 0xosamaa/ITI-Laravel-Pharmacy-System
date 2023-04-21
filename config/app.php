428b5583dd9b74b68a9aef5392914a7e896b9dc0
Osama <0xm.osamaa@gmail.com>
2023-04-21 14:25:31 +0200

    removed countries and added governorates

--- config/app.php
+++ config/app.php
@@ -168,7 +168,6 @@ return [
         // App\Providers\BroadcastServiceProvider::class,
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
-        'Webpatser\Countries\CountriesServiceProvider',
     ])->toArray(),
 
     /*
@@ -184,7 +183,6 @@ return [
 
     'aliases' => Facade::defaultAliases()->merge([
         // 'Example' => App\Facades\Example::class,
-        'Countries' => 'Webpatser\Countries\CountriesFacade',
     ])->toArray(),
 
 ];
