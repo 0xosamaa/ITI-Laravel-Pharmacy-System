cc610f001cfd25f3b541c9cdda7557a2c66b0b2d
Osama <0xm.osamaa@gmail.com>
2023-04-21 21:04:34 +0200

    medicine seeder with categories and discounts

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,6 +3,8 @@
 namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+
+use App\Models\Category;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -23,6 +25,15 @@ class DatabaseSeeder extends Seeder
 
         $this->call(AdminSeeder::class);
         $this->command->info('Seeded the Admins!');
+
+        $this->call(CategorySeeder::class);
+        $this->command->info('Seeded the Categories!');
+
+        $this->call(DiscountSeeder::class);
+        $this->command->info('Seeded the Discounts!');
+
+        $this->call(MedicineSeeder::class);
+        $this->command->info('Seeded the Medicines!');
 
         $this->call(PharmacySeeder::class);
         $this->command->info('Seeded the Pharmacies!');
--- database/seeders/MedicineSeeder.php
+++ database/seeders/MedicineSeeder.php
@@ -2,6 +2,8 @@
 
 namespace Database\Seeders;
 
+use App\Models\Category;
+use App\Models\Discount;
 use App\Models\Medicine;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
@@ -13,5 +15,20 @@ class MedicineSeeder extends Seeder
      */
     public function run(): void
     {
+        $discounts = Discount::all()->pluck('id');
+        $categories = Category::all()->pluck('id');
+
+
+        for ($i = 0; $i < 100; $i++) {
+            Medicine::create([
+                'name' => fake()->name(),
+                'description' => fake()->text(),
+                'image' => 'site/images/product_0' . fake()->numberBetween(1, 6) . '.png',
+                'SKU' => fake()->numberBetween(20000, 90000),
+                'price' => fake()->numberBetween(5000, 100000),
+                'discount_id' => $discounts->random(),
+                'category_id' => $categories->random(),
+            ]);
+        }
     }
 }
