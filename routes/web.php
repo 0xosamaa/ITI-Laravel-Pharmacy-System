6cbd34a7e415cf5acac1551c026f765c4c9321b2
Osama <0xm.osamaa@gmail.com>
2023-04-21 21:07:42 +0200

    shop, landing page, medicines routes

--- routes/web.php
+++ routes/web.php
@@ -1,14 +1,17 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ShopController;
 use App\Http\Controllers\DoctorController;
 use App\Http\Controllers\ProfileController;
 use App\Http\Controllers\Admin\RoleController;
 use App\Http\Controllers\Admin\OrderController;
-use App\Http\Controllers\Admin\GovernorateController;
+use App\Http\Controllers\LandingPageController;
 use App\Http\Controllers\Admin\PharmacyController;
 use App\Http\Controllers\Admin\DashboardController;
 use App\Http\Controllers\Admin\PermissionController;
+use App\Http\Controllers\Admin\GovernorateController;
+use App\Http\Controllers\Admin\MedicineController;
 
 /*
 |--------------------------------------------------------------------------
@@ -21,9 +24,7 @@ use App\Http\Controllers\Admin\PermissionController;
 |
 */
 
-Route::get('/', function () {
-    return view('site.landing-page');
-})->name('landing-page');
+Route::get('/', [LandingPageController::class, 'index'])->name('site.landing-page');
 
 Route::get('/admin', function () {
     return redirect()->route('admin.dashboard');
@@ -35,7 +36,7 @@ Route::get('/dashboard', function () {
 
 
 
-Route::middleware(['auth', 'verified','role:admin|doctor|pharmacist'])->name('admin.')->prefix('admin')->group(function () {
+Route::middleware(['auth', 'verified', 'role:admin|doctor|pharmacist'])->name('admin.')->prefix('admin')->group(function () {
 
     Route::get('/dashboard', [DashboardController::class, 'index'])->name('dashboard');
     //roles
@@ -48,19 +49,24 @@ Route::middleware(['auth', 'verified','role:admin|doctor|pharmacist'])->name('ad
     Route::resource('/pharmacies', PharmacyController::class);
     //governorates
     Route::resource('/governorates', GovernorateController::class);
-
-
+    //medicines
+    Route::resource('/medicines', MedicineController::class);
 });
 
 
 
 
-Route::middleware(['auth', 'role:admin|pharmacist'])->group(function() {
+Route::middleware(['auth', 'role:admin|pharmacist'])->group(function () {
     Route::get('/doctors', [DoctorController::class, 'index'])->name('doctors.index');
     Route::get('/doctors/create', [DoctorController::class, 'create'])->name('doctors.create');
     //doctors
     // Route::resource('/doctors', DoctorController::class);
 
+});
+
+Route::name('site.')->group(function () {
+    Route::get('/shop', [ShopController::class, 'index'])->name('shop.index');
+    Route::get('/shop/{medicine:slug}', [ShopController::class, 'show'])->name('shop.show');
 });
 
 Route::middleware('auth')->group(function () {
@@ -70,4 +76,3 @@ Route::middleware('auth')->group(function () {
 });
 
 require __DIR__ . '/auth.php';
-
