60c6518f8eb79adda9176978365a85835661edd2
mostafa2080 <mostafashaaban2080@gmail.com>
2023-04-22 08:37:44 +0200

    minor chnage in pharmacy display

--- resources/views/admin/pharmacies/edit.blade.php
+++ resources/views/admin/pharmacies/edit.blade.php
@@ -24,23 +24,32 @@
 
                             <div class="form-group">
                                 <label for="owner_user_id">Owner User ID</label>
-                                <input id="owner_user_id" type="number" class="form-control @error('owner_user_id') is-invalid @enderror" name="owner_user_id" value="{{ old('owner_user_id', $pharmacy->owner_user_id) }}" required>
+                                <select name="owner_user_id" id="owner_user_id" class="form-control @error('owner_user_id') is-invalid @enderror" required>
+                                    <option value="">Select an owner user</option>
+                                    @foreach($users as $user)
+                                        <option value="{{ $user->id }}" {{ old('owner_user_id') == $user->id ? 'selected' : '' }}>
+                                            {{ $user->name }} ({{ $user->email }})
+                                        </option>
+                                    @endforeach
+                                </select>
                                 @error('owner_user_id')
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
+                                    <span class="invalid-feedback">{{ $message }}</span>
                                 @enderror
                             </div>
 
                             <div class="form-group">
                                 <label for="governorate_id">Governorate</label>
-                                <input id="governorate_id" type="number" class="form-control @error('governorate_id') is-invalid @enderror" name="governorate_id" value="{{ old('governorate_id', $pharmacy->governorate_id) }}" required>
+                                <select name="governorate_id" id="governorate_id" class="form-control @error('governorate_id') is-invalid @enderror" required>
+                                    <option value="">Select Governorate</option>
+                                    @foreach($governorates as $governorate)
+                                        <option value="{{ $governorate->id }}" {{ old('governorate_id') == $governorate->id ? 'selected' : '' }}>{{ $governorate->name }}</option>
+                                    @endforeach
+                                </select>
                                 @error('governorate_id')
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
+                                    <span class="invalid-feedback">{{ $message }}</span>
                                 @enderror
                             </div>
+
 
                             <div class="form-group">
                                 <label for="name">Name</label>
