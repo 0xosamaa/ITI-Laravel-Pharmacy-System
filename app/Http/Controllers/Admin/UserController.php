1f0fe168b1d3bf42eda9ce773e92525a03f98cbb
Muhammed-saber79 <muhammed.saber385@gmail.com>
2023-04-24 15:24:14 +0200

    modify users again

--- app/Http/Controllers/Admin/UserController.php
+++ app/Http/Controllers/Admin/UserController.php
@@ -74,6 +74,7 @@ class UserController extends Controller
             $user->national_id = $validData['national_id'];
             $user->gender = $validData['gender'];
             $user->profile_image_path = $fileName;
+            // $user->assignRole('user');
             $user->save();
 
             $address = new UserAddress;
@@ -162,5 +163,6 @@ class UserController extends Controller
 
     public function destroy($id){
         User::destroy($id);
+        return redirect()->route('admin.users.index')->with('success', 'User deleted successfully.');
     }
 }
