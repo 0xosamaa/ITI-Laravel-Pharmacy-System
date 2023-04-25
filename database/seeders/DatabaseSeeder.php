ae9601d96cf8fd6d69b8ef59c38b286fd3a4d6bd
Osama <0xm.osamaa@gmail.com>
2023-04-26 01:46:34 +0200

    deleted CartSeeder (no longer needed)

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -45,9 +45,6 @@ class DatabaseSeeder extends Seeder
         $this->call(DoctorSeeder::class);
         $this->command->info('Seeded the Doctors!');
 
-        // $this->call(CartSeeder::class);
-        // $this->command->info('Seeded the Carts!');
-
         $this->call(OrderDetailsSeeder::class);
         $this->command->info('Seeded the Order Details!');
     }
