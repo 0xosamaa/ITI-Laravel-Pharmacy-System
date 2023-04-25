5a3a51a7cc3ba412c10cc6b4acd2d96efb213cf9
hassanOsama720 <hassan.osama720@gmail.com>
2023-04-25 16:27:07 +0200

    Make Api CRUD For orders

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\API\OrderController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -17,3 +18,9 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+// authenticated routes to add new order
+//Route::middleware('auth:sanctum')->group(function () {
+    Route::post('/order', [OrderController::class, 'AddNewOrder']);
+//});
+
